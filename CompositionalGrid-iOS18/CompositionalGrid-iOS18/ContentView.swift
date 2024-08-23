afc647ccb611c93cc7b9742c416d416c5d6c9101
Koki Yamada <koki.yamada@link-u.co.jp>
2024-08-23 23:23:09 +0900

    Add: Compositional Grid

--- CompositionalGrid-iOS18/CompositionalGrid-iOS18/ContentView.swift
+++ CompositionalGrid-iOS18/CompositionalGrid-iOS18/ContentView.swift
@@ -1,14 +1,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count: Int = 3
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 6) {
+                    PickerView()
+                        .padding(.bottom, 10)
+
+                    CompositionalLayout {
+                        ForEach(1...50, id: \.self) { index in
+                            Rectangle()
+                                .fill(.black.gradient)
+                                .overlay {
+                                    Text("\(index)")
+                                        .foregroundStyle(.white)
+                                        .font(.largeTitle.bold())
+                                }
+                        }
+                    }
+                    .animation(.bouncy, value: count)
+                }
+                .padding(15)
+            }
+            .navigationTitle("Compositional Grid")
         }
-        .padding()
+    }
+
+    @ViewBuilder
+    func PickerView() -> some View {
+        Picker("", selection: $count) {
+            ForEach(1...4, id: \.self) {
+                Text("Count = \($0)")
+                    .tag($0)
+            }
+        }
+        .pickerStyle(.segmented)
     }
 }
 
